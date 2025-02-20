6c013c31ee0e3c93125cd42db5ac6b24415e5d9b
Rinty54 <gregory_benoit@hotmail.com>
2025-02-20 20:40:01 +0100

    Update alpine-nextcloud.sh

--- ct/alpine-nextcloud.sh
+++ ct/alpine-nextcloud.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
-# Copyright (c) 2021-2024 tteck
-# Author: tteck (tteckster)
+source <(curl -s https://raw.githubusercontent.com/Rinty54/Proxmox/main/misc/build.func)
+# Copyright (c) 2021-2024 Rinty54
+# Author: Rinty54 (Rinty54ster)
 # License: MIT
-# https://github.com/tteck/Proxmox/raw/main/LICENSE
+# https://github.com/Rinty54/Proxmox/raw/main/LICENSE
 
 function header_info {
   clear
